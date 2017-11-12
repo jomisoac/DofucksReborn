83e2ffcc9f7de00167911d3a67a774fddc81ad2d
SwiTool <switooldev@gmail.com>
2017-11-12 19:05:11 +0100

    items list search on deleter

--- src/client/bot/modules/deleter/ItemsToDelete.js
+++ src/client/bot/modules/deleter/ItemsToDelete.js
@@ -1,60 +1,19 @@
 import React from 'react';
 
-import Module from '../../Module';
-import TextField from 'material-ui/TextField';
+import ItemSearcher from '../../util/ItemSearcher';
 
-class ItemsToDelete extends Module {
+class ItemsToDelete extends React.Component {
   constructor(props) {
-    super(props),
-    this.tag = "deleter_items_to_delete";
-    this.state._[this.tag] = [];
-    this.state.value = '';
+    super(props)
   }
 
-  ready() {
+  setItems(items) {
+    this.props.win.Dofucks.Deleter.wantToDelete = items;
   }
-
-  setItemsToDelete(v) {
-    this.props.win.Dofucks.Deleter.wantToDelete = v;
-  }
-
-  onLoaded(data) {
-    if (data) {
-      this.setItemsToDelete(data);
-      this.setState({
-        value: data.toString()
-      });
-    }
-    return data;
-  }
-
-  handleChange(event, nv) {
-    var v = this.props.win.Dofucks.Utils.getCommaSeparatedNumbers(nv);
-    if (nv && nv[nv.length - 1] !== ',') {
-      this.setState({
-        value: v.toString()
-      });
-      this.setValue(v);
-      this.setItemsToDelete(v);
-    } else {
-      this.setState({
-        value: nv
-      });
-    }
-  };
 
   render() {
     return (
-      <TextField
-        floatingLabelText="Items to delete"
-        floatingLabelFixed={true}
-        hintText="1,2,3"
-        value={this.state.value}
-        onChange={this.handleChange.bind(this)}
-        fullWidth={true}
-        multiLine={true}
-        rowsMax={5}
-      />
+      <ItemSearcher win={this.props.win} tag="deleter_items_to_delete" setItems={this.setItems.bind(this)}/>
     );
   }
 }
--- src/client/bot/modules/seller/ItemsToSell.js
+++ src/client/bot/modules/seller/ItemsToSell.js
@@ -1,113 +1,19 @@
 import React from 'react';
 
-import Module from '../../Module';
-import Item from './ItemsToSell/Item';
-import TextField from 'material-ui/TextField';
-import AutoComplete from 'material-ui/AutoComplete';
-import { Grid, Row, Cell } from 'react-inline-grid';
+import ItemSearcher from '../../util/ItemSearcher';
 
-import axios from 'axios';
-
-class ItemsToSell extends Module {
+class ItemsToSell extends React.Component {
   constructor(props) {
-    super(props),
-    this.tag = "seller_items_to_sell";
-    this.state._[this.tag] = [];
-    this.state.searchText = '';
-    this.state.items = [];
+    super(props)
   }
 
-  ready() {
-  }
-
-  setItemsToSell(v) {
-    this.props.win.Dofucks.Seller.wantToSell = v;
-  }
-
-  onLoaded(data) {
-    if (data) {
-      data = data.filter((e) => {
-        return typeof e == "object"
-      })
-      var items = data.map(e => e.value);
-      this.setItemsToSell(items);
-    }
-    return data;
-  }
-
-  fetchItems() {
-    if (this.state.searchText.length < 3) return;
-    axios.get('http://dofucks.com:8000/search/items?q='+this.state.searchText+'&l='+this.props.win.Config.language)
-    .then(r => {
-      var items = [];
-      r.data.forEach((e, i) => {
-        items.push({text: e.nameId, value: e.id, iconId: e.iconId});
-      })
-      this.setState({items: items});
-    });
-  }
-
-  handleUpdateInput(searchText) {
-    this.setState({
-      searchText: searchText,
-    }, () => {
-      this.fetchItems();
-    });
-  }
-
-  handleNewRequest(item) {
-    var oldItems = this.state._[this.tag];
-    var found = false;
-    oldItems.forEach(e => {
-      if (item.value == e.value) found = true;
-    })
-    if (!found) {
-      oldItems.push(item);
-      this.saveData(oldItems);
-    }
-    this.setState({
-      searchText: '',
-      items: []
-    });
-  }
-
-  onItemClick(itemId) {
-    var items = this.state._[this.tag];
-    var newItems = items.filter(e => e.value != itemId);
-    this.saveData(newItems);
-  }
-
-  saveData(newItems) {
-    var items = newItems.map(e => e.value)
-    this.setValue(newItems);
-    this.setItemsToSell(items);
+  setItems(items) {
+    this.props.win.Dofucks.Seller.wantToSell = items;
   }
 
   render() {
     return (
-      <div>
-        <AutoComplete
-          hintText="Type the name of an item"
-          searchText={this.state.searchText}
-          onUpdateInput={this.handleUpdateInput.bind(this)}
-          onNewRequest={this.handleNewRequest.bind(this)}
-          dataSource={this.state.items}
-          filter={(searchText, key) => (key.toLowerCase().indexOf(searchText.toLowerCase()) !== -1)}
-          openOnFocus={true}
-          fullWidth={true}
-        />
-        <Grid>
-          <Row is="nospace">
-            {
-              this.state._[this.tag].map((e, i) => {
-                return (
-                  <Item key={i} id={e.value} iconId={e.iconId} onItemClick={this.onItemClick.bind(this)}/>
-                )
-              })
-            }
-          </Row>
-        </Grid>
-    </div>
+      <ItemSearcher win={this.props.win} tag="seller_items_to_sell" setItems={this.setItems.bind(this)}/>
     );
   }
 }
